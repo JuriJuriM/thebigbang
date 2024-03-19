3c13ac63d5c4ff66909f51843c609b24fba4475b
JuriJuriM <jurimudry@gmail.com>
2024-03-19 19:28:56 +0300

    my first commit

---
+++ src/components/pages/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Main.scss'
+
+const Main = () => {
+    return (
+        <div className='main'>
+            <div className='container'>
+                <div className='main__body'>
+                    <div className='main__title'>Main page</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
---
+++ src/components/pages/Second.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Second.scss'
+
+const Second = () => {
+    return (
+        <div className='second'>
+            <div className='container'>
+                <div className='second__body'>
+                    <div className='second__title'>Second page</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Second
