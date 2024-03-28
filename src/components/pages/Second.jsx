b0783357cca37987cfd53c9d4709a610d4d581c5
JuriJuriM <jurimudry@gmail.com>
2024-03-28 18:37:06 +0300

    new comment here

--- src/components/pages/Second.jsx
+++ src/components/pages/Second.jsx
@@ -1,12 +1,230 @@
 import React from 'react'
 import './Second.scss'
+import Allheroes from '../../assets/img/Allheroes.jpg'
+import Leo from '../../assets/img/leo.jpg'
+import Leopeny from '../../assets/img/Leopeny.jpg'
+import Sheldon from '../../assets/img/Sheldon.jpg'
+import Sheldonemy from '../../assets/img/Sheldonemy.jpg'
+import Peny from '../../assets/img/Penny.jpg'
+import Penyleo from '../../assets/img/Pennyleo.jpg'
+import Hovard from '../../assets/img/Hovard.jpg'
+import Hovardberny from '../../assets/img/Hovardberny.jpg'
+import Berny from '../../assets/img/berny.jpg'
+import Bernyhovard from '../../assets/img/Bernyhovard.jpg'
+import Emy from '../../assets/img/Emy.jpg'
+import Emysheldon from '../../assets/img/Emysheldon.jpg'
+import Radgesh from '../../assets/img/Radgesh.jpg'
+import Radgeshtwo from '../../assets/img/Radgeshtwo.jpg'
+import Tbbt from '../../assets/img/tbbt.jpg'
 
 const Second = () => {
     return (
         <div className='second'>
             <div className='container'>
                 <div className='second__body'>
-                    <div className='second__title'>Second page</div>
+                    <div className='second__title'>The main heroes of The Big Bang Theory</div>
+                    <div className='second__one'>
+                        <img src={Allheroes} alt='Allheroes' />
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Leo} alt='Leo' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Johnny Galecki as Leonard Hofstadter: An experimental physicist with an IQ of 173, who received his Ph.D. when he was 24 years old. Leonard is a nerd who loves video games, comic books, and Dungeons & Dragons. Leonard is the straight man of the series, sharing an apartment in Pasadena, CA, with Sheldon Cooper.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Leonard is smitten with his new neighbor Penny when they first meet, and they eventually marry.</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Leopeny} alt='Leopeny' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Sheldon} alt='Sheldon' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Jim Parsons as Sheldon Cooper: Originally from Galveston, Texas, Sheldon was a child prodigy with an eidetic memory who began college at the age of eleven and earned a Ph.D. at age sixteen. He is a theoretical physicist researching quantum mechanics and string theory, and, despite his IQ of 187, he finds many routine aspects of social situations difficult to grasp.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>He is determined to have his own way, continually boasts of his intelligence, and has an extremely ritualized way of living. Despite these quirks, he begins a relationship with Amy Farrah Fowler, and they eventually marry.</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Sheldonemy} alt='Sheldonemy' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Peny} alt='Peny' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Kaley Cuoco as Penny: An aspiring actress from Omaha, Nebraska. Penny moves in across the hall from Sheldon and Leonard. She waits tables and occasionally tends the bar at The Cheesecake Factory. After giving up hope of becoming a successful actress, Penny becomes a pharmaceutical sales representative.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Penny becomes friends with Bernadette and Amy, and they often hang out in each other's apartments. Penny and Leonard form a relationship and eventually marry.</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Penyleo} alt='Penyleo' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Hovard} alt='Hovard' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Simon Helberg as Howard Wolowitz: An aerospace engineer who got his master's degree at the Massachusetts Institute of Technology. Howard is Jewish and lived with his mother, Debbie (Carol Ann Susi). Unlike Sheldon, Leonard, Raj, Bernadette, and Amy, Howard does not hold a doctorate.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>He trains as an astronaut and goes into space as a payload specialist on the International Space Station. Howard initially fancies himself as a ladies man, but he later starts dating Bernadette, and they get engaged and married.</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Hovardberny} alt='Hovardberny' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Berny} alt='Berny' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Melissa Rauch as Bernadette Rostenkowski-Wolowitz (recurring season 3, starring seasons 4–12): A young woman who initially is a co-worker at The Cheesecake Factory with Penny to pay her way through graduate school, where she is studying microbiology.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Bernadette is introduced to Howard by Penny; at first, they do not get along, apparently having nothing in common. They date and later get engaged and married. Although generally a sweet and good-natured person, Bernadette has a short fuse and can be vindictive and lash out when provoked.</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Bernyhovard} alt='Bernyhovard' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Emy} alt='Emy' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Mayim Bialik as Amy Farrah Fowler (guest star season 3, starring seasons 4–12): A woman selected by an online dating site as Sheldon's perfect mate, Amy is from Glendale, California. While she and Sheldon initially share social cluelessness, after befriending Penny and Bernadette, she eventually becomes more interested in social and romantic interaction.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Her relationship with Sheldon slowly progresses to the point at which Sheldon considers her his girlfriend, and eventually, they get married. Amy believes she and Penny are best friends, a sentiment that Penny does not initially share. Amy has a Ph.D. in neurobiology.</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Emysheldon} alt='Emysheldon' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Radgesh} alt='Radgesh' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Kunal Nayyar as Rajesh Koothrappali: A particle astrophysicist originally from New Delhi, India. Initially, Raj had selective mutism, rendering him unable to talk to or be around women unless under the influence of alcohol. He also has a Yorkshire Terrier named Cinnamon.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>He later speaks to Penny without alcohol, overcoming his selective mutism. He begins dating Emily Sweeney, and their relationship later becomes exclusive. In the series' final season, Raj has an on-again, off-again engagement with a fellow Indian, a hotel concierge named Anu (Rati Gupta).</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Radgeshtwo} alt='Radgeshtwo' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__one'>
+                        <img src={Tbbt} alt='Tbbt' />
+                    </div>
                 </div>
             </div>
         </div>
